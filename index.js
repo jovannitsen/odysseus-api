1abb7050829efb810f6b46c58f86956bfe145cb2
stoodkev <quentin.correa@gmail.com>
2021-06-03 10:51:47 +0800

    work with secure hosting de

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 var bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,32 +13,37 @@ var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(jsonParser);
 app.use(urlencodedParser);
-
+app.use(cors());
 app.post("/contact", ({ body }, res) => {
   const { name, email, message } = body;
-  console.log("receiving contact");
   var transporter = nodemailer.createTransport({
     host: "mail.hosting.de",
-    port: 587,
+    port: 25,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: process.env.USER,
-      pass: process.env.PWD,
+      user: process.env.FROM_EMAIL,
+      pass: process.env.PWD_EMAIL,
     },
   });
 
   var mailOptions = {
-    from: process.env.FROM_EMAIL,
+    from: { name: "Odysseus Info", address: process.env.FROM_EMAIL },
     to: process.env.TO_EMAIL,
+    replyTo: email,
     subject: `Contact from ${name}`,
-    html: `<p><b>Contact name :</b> ${name}</p><p><b>Contact email :</b> ${email}</p><p><b>Message :</b></p><p>${name}</p>`,
+    html: `<p><b>Contact name :</b> ${name}</p><p><b>Contact email :</b> ${email}</p><p><b>Message :</b></p><p>${message}</p>`,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      res.status(500);
+      res.status(500).send();
     } else {
-      res.status(200);
+      var response = {
+        status: 200,
+        success: "Updated Successfully",
+      };
+
+      res.end(JSON.stringify(response));
     }
   });
 });
